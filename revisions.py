e38316d1f9027269b7f0e310bcbf716b1b0ca890
kol67658 <kolj23131@wainuiomatahigh.school.nz>
2025-04-15 13:00:56 +1200

    Update revisions.py
    3 april 9.57

--- revisions.py
+++ revisions.py
@@ -1,1 +1,26 @@
+# Title
+print('Uproar')
+print('By Joe Koloa')
+
+
+
+# Variables Output
+questions = ['q1', 'q2', 'q3' , 'q4' , 'q5' , 'q6']
+points = [6]
+
+
+
+print('Hello there, welcome to my quiz about the 2023 NZ featured film "Uproar" please answer 1 out of 6 questions')
+
+input('To start, please enter your username and age')
+
+input('What is your name'?'')
+int')'input)) = (name)
+
+input ('What is your age?')
+int')'input)) = (age)
+
+
+print('Question 1')
+print('Who is the Director of the film?')
 
