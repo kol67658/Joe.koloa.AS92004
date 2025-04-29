545460b5e9b880a1affab99dcb12565aca260ab2
kol67658 <kolj23131@wainuiomatahigh.school.nz>
2025-04-29 21:03:40 +1200

    Update main.py

--- main.py
+++ main.py
@@ -101,6 +101,116 @@ print("You got" + int(points) + " questions correct!")
 
 
 
+# Title
+print('Uproar')
+print('By Joe Koloa')
+
+import time
+import os
+import time
+
+
+# Variables Output
+str_playername = ""
+questions = ['q1', 'q2', 'q3' , 'q4' , 'q5' , 'q6']
+points = 0
+play_again = True 
+answer = ''
+correct = (f"Correct, you now have {points + 1} points")
+incorrect = (f" Wrong, you now have 0/6 points") 
+results = (f'You got {points} out of 6 questions')
+
+time.sleep(1)
+
+print(f'Hello player, welcome to my quiz about the 2023 NZ featured film "Uproar", please answer 0 out of 6 questions') 
+print(f'IF you made a mistake, that is ok, as long you answer each questions. But each questions has a timer, ending in 20 seconds')
+print(f'To start, please enter your username')
+
+print('What is your name?')
+str_playername = input()
+
+print('Welcome {}, please press enter'.format(str_playername))
+
+
+print('Question 1')
+print('Who is the Director of the film? \n a. Hamish Bennet \n b. Cliff Curtis \n c. Peter Jackson \n d. Jemaine Clement ')
+answer = input('')
+if answer == 'Hamish Bennet' or 'a':
+    points =+ 1
+    print(correct)
+
+else:
+    print(incorrect)
+
+
+print('Questions 2')
+print('Who play as John Waaka?\nHint: He appeared in the Hunter of the wilderpeople \n a. Julian Dennison \n b. Uli Latukefu \n c. KJ APA \n d. Martinson Henderson' )
+
+answer = answer.lower()
+
+if answer == 'Julian Dennison' or 'a':
+    print(correct)
+    points =+ 1
+else:
+    print(incorrect)
+    
+    
+
+print('Question 3')
+print('Who played as Jamie Waaka , john brother? hint: He played as alemein in boy \n a. Daniel logan \n b. James Rolleston \n c. Kaimana')
+
+
+answer = answer.lower()
+
+if answer == 'James Rolleston' or 'b':
+   print(correct)
+   points += 1
+
+else:
+    print(incorrect)
+  
+   
+print('Question 4')
+print('Which clip of the film is based on one of the true event in 1981? \n a. Springbok tour protest \n b. Queen Street riot \n  c. Montego Bay \n d. All black won the world cup ')
+answer = answer.lower()
+
+if answer == 'Springbok tour protest' or 'a':
+   print(correct)
+   points += 1
+else:
+    print(incorrect)
+    
+    
+  
+print('Question 5')
+print('Julian Dennison is an experienced rugby player in real life, True or False')
+
+answer = answer.lower()
+
+bool_ans1 = True
+bool_ans2 = False
+
+if answer == 'True':
+    print('Wow, one more to go , 5/6')
+    points += 1
+else:
+    print('OOf, sorry wrong again, 0/6')
+
+print('Question 6')
+print('What do you think of my game, rating out of 100? 20, 60, 75, or 100?')
+
+answer = input()
+
+if answer == '20' or '60' or '75' or '100':
+    print('Wow, Thank you so much for playing my game , 6/6')
+    points += 1
+    print('Here is your prize')
+    print('You have received $200,000,000')
+else:
+    print(incorrect)
+    
+print("You got" + int(points) + " out of 6 questions correct!")
+
 
 
 
